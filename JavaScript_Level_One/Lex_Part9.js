41f50f29dc94b6b2a205a2de32fbd62c212d11c5
Lex Myers <lex.myers@gmail.com>
2017-03-22 15:27:43 -0700

    added prompts for variables

---
+++ JavaScript_Level_One/Lex_Part9.js
@@ -0,0 +1,5 @@
+var firstName = prompt('Please enter your first name:');
+var lastName = prompt('Please enter your last name:');
+var age = prompt('Please enter your age:');
+var height = prompt('Now, your height (in centimeters)');
+var petName = prompt('How about the name of your pet?');
