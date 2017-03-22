464fde2ec3e295cebf26426aa3e233ad9cff7fee
Lex Myers <lex.myers@gmail.com>
2017-03-22 15:29:23 -0700

    added thank you alert

--- JavaScript_Level_One/Lex_Part9.js
+++ JavaScript_Level_One/Lex_Part9.js
@@ -3,3 +3,4 @@ var lastName = prompt('Please enter your last name:');
 var age = prompt('Please enter your age:');
 var height = prompt('Now, your height (in centimeters)');
 var petName = prompt('How about the name of your pet?');
+alert('Thanks for your answers!');
