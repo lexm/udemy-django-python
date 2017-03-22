4f36fe1ad89765e23f296c23cacc7cd0f7ead9cd
Lex Myers <lex.myers@gmail.com>
2017-03-22 15:33:59 -0700

    now, a message for the spy

--- JavaScript_Level_One/Lex_Part9.js
+++ JavaScript_Level_One/Lex_Part9.js
@@ -3,4 +3,11 @@ var lastName = prompt('Please enter your last name:');
 var age = prompt('Please enter your age:');
 var height = prompt('Now, your height (in centimeters)');
 var petName = prompt('How about the name of your pet?');
+if(firstName[0] === lastName[0]
+   && age > 20
+   && age < 30
+   && height >= 170
+   && petName[petName.length - 1] === 'y') {
+     console.log('Hello, Spy!');
+   }
 alert('Thanks for your answers!');
